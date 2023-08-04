07c33adc5cfe0a08173c746f9d909046afa28784
Michelle Harley <chell.hx@gmail.com>
2023-08-04 19:57:09 -0400

    Home component skeleton done

---
+++ src/components/Home/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="Home">
+      <h1>
+        Welcome To
+        <br />
+        The LunchBox
+      </h1>
+      <br />
+      <img
+        src="https://cdn.dribbble.com/users/692029/screenshots/5268418/media/1bfe66e050b08076691c2eefacfe4e01.gif"
+        alt="Snacks holding hands"
+      />
+    </div>
+  );
+}
+
+export default Home;
--- src/components/Snack/Snack.js
+++ src/components/Snack/Snack.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './Snack.css'
 
 function Snack() {
   return (
