bfb70f137c17f4e9d6d18cef47426bef1f5473b9
Angel <angel@Angels-MacBook-Air.local>
2023-08-04 23:03:27 -0400

    completed snacks.js

---
+++ src/components/Snacks/Snacks.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Snacks.css";
+
+function Snacks() {
+  const [snacks, setSnacks] = useState([]);
+  let url = process.env.REACT_APP_API_URL;
+
+  async function fetchSnacksData() {
+    try {
+      let result = await axios.get(`${url}/snacks`);
+
+      setSnacks(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchSnacksData();
+  }, []);
+  return (
+    <div>
+      <h1 className="snacks-container-index">Index</h1>
+      <div className="snacks-container">
+        <div className="snacks-container-table">
+          <table id="snacks">
+            <tbody>
+              <tr>
+                <th>Date</th>
+                <th>Name</th>
+                <th>Rating</th>
+                <th>Favorite</th>
+              </tr>
+              {snacks.map((snack) => {
+                return (
+                  <tr key={snack.id}>
+                    <td>{snack.released_date}</td>
+                    <td id="snacks-name">
+                      <Link to={`/snacks/${snack.id}`}>{snack.name}</Link>
+                    </td>
+                    <td>{snack.rating}</td>
+                    <td>{snack.is_favorite ? "🍘" : ""}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Snacks;
