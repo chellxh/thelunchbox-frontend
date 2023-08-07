bf65ee42d97298cea14a2f4b131c385f3e936658
Michelle Harley <chell.hx@gmail.com>
2023-08-07 11:52:04 -0400

    home background color change

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,18 +3,16 @@ import "./Home.css";
 
 function Home() {
   return (
+    <div className="homeDiv">
+  <h1 className="welcome">Welcome To The LunchBox</h1>
     <div className="Home">
-      <h1>
-        Welcome To
-        <br />
-        The LunchBox
-      </h1>
+      
       <br />
       <img
         src="https://thumbs.gfycat.com/WeeImpeccableElephantseal.webp"
         alt="Bento box of lunch"
       />
-    </div>
+    </div></div>
   );
 }
 
