853bbbb1a7ad5e735233bb1ca2480b00969c2c6f
Michelle Harley <chell.hx@gmail.com>
2023-08-07 21:22:58 -0400

    finished home.css, edited snack.css, and added an about section to the home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,17 +4,21 @@ import "./Home.css";
 function Home() {
   return (
     <div className="homeDiv">
-  <h1 className="welcome">Welcome To The LunchBox</h1>
-    <div className="Home">
-      
-      <br />
-      <img
-        src="https://thumbs.gfycat.com/WeeImpeccableElephantseal.webp"
-        alt="Bento box of lunch"
-      />
-    </div></div>
+      <h1 className="welcome">Welcome To The LunchBox</h1>
+      <div className="Home">
+        <img
+          src="https://thumbs.gfycat.com/WeeImpeccableElephantseal.webp"
+          alt="Bento box of lunch"
+        />
+      </div>
+      <div className="about">
+        <p>
+          Where all snacks are munchie material. Don't see your favorite snack?
+          Add it to the lunchbox and let's get snackin'.
+        </p>
+      </div>
+    </div>
   );
 }
 
 export default Home;
-
