5bb383dc092a57605621289692ec16724c666e1b
ambarimar ulloa <ambarimar@ambarimars-Air.fios-router.home>
2023-08-09 12:58:41 -0400

    added function for calculating rating with emogi

--- src/components/Snacks/Snacks.js
+++ src/components/Snacks/Snacks.js
@@ -20,6 +20,19 @@ function Snacks() {
   useEffect(() => {
     fetchSnacksData();
   }, []);
+
+
+  function calculateRatingEmogi(rating) {
+    if (rating === 5 || rating >= 4) {
+      return "🤤";
+    } else if (rating === "3") {
+      return "😑";
+    } else if (rating <= 2) {
+      return "🤮";
+    }
+  }
+
+
   return (
     <div>
       <h1 className="snacks-container-index">Index</h1>
@@ -46,7 +59,7 @@ function Snacks() {
                     <td id="snacks-name">
                       <Link to={`/snacks/${snack.id}`}>{snack.name}</Link>
                     </td>
-                    <td>{snack.rating}</td>
+                    <td>{calculateRatingEmogi(snack.rating)}</td>
                     <td>{snack.is_favorite ? "🍘" : ""}</td>
                   </tr>
                 );
